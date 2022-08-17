c178ce43505240d6bd6ed2a2758d33da0aa92a97
Jackson Morris-Thring <jacksonmorris-thring@student-10-201-33-076.wireless.adelaide.edu.au>
2022-08-17 12:47:57 +0930

    1-1 DONE

---
+++ function-1-1.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <iostream>
+
+void string_2d_copy(std::string first[][2], std::string second[][2], int n){
+    int cols = 2;
+    int rows = n;
+
+    for(int i = 0; i < rows; i++){
+        for(int j = 0; j < cols; j++){
+            second[i][j] = first[i][j];
+        }
+    }
+
+    for(int i = 0; i < rows; i++){
+        for(int j = 0; j < cols; j++){
+            if(j == cols - 1){
+                std::cout << second[i][j] << std::endl;
+            }
+            else{
+                std::cout << second[i][j] << " ";
+            }
+            
+        }
+    }
+
+
+
+}
---
+++ main-1-1.cpp
@@ -0,0 +1,16 @@
+#include "function-1-1.cpp"
+#include <iostream>
+
+int main(){
+    std::string array1[3][2] = {
+        {"John", "Peter"},
+        {"John", "Peter"},
+        {"John", "Peter"},
+    };
+
+    std::string array2[3][2];
+
+    string_2d_copy(array1, array2, 3);
+
+    return 0;
+}
