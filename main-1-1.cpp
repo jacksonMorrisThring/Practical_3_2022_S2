8cf23975ebc9819e4040056b242df21e2932a53e
Jackson Morris-Thring <jacksonmorris-thring@student-10-201-33-076.wireless.adelaide.edu.au>
2022-08-17 13:34:50 +0930

    1-1

--- main-1-1.cpp
+++ main-1-1.cpp
@@ -1,5 +1,6 @@
-extern void string_2d_copy(std::string first[][2], std::string second[][2], int n);
+#include <string>
 #include <iostream>
+extern void string_2d_copy(std::string first[][2], std::string second[][2], int n);
 
 int main(){
     std::string array1[3][2] = {
