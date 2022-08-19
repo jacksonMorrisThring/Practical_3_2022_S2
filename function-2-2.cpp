3501db1b3ba15b0e1eeece3253bb28fc65f46d16
Jackson Morris-Thring <jacksonmorris-thring@Jacksons-MacBook-Air-2.local>
2022-08-19 14:54:19 +0930

    fixed 2-2

--- function-2-2.cpp
+++ function-2-2.cpp
@@ -2,24 +2,40 @@
 
 int max_sub_sum(int *nums,int length){
     int max_sub = 0;
-    int temp = 0;
+
+    int index1;
+    int index2;
     
+    std::cout << "\nLength: " << length << std::endl;
     for(int i = 0; i < length; i++){
-        for(int j = i + 1; j < length - 1; j++){
-            if(j == 1){
-                temp = *(nums + i) + *(nums + j);
+        int sum = 0;
+        std::cout << std::endl;
+        for(int j = i + 1; j < length; j++){
+            std::cout << "Value of i: " << i << "   Value of j: " << j << std::endl; 
+            if(j == i + 1){
+                index1 = i;
+                index2 = j;
+                sum = sum + *(nums + index1) + *(nums + index2);
+                // temp = *(nums + i) + *(nums + j);
             }
             else{
-                temp = temp + *(nums + j);
+                index1 = i;
+                int len = j-i + 1;
+
+                for(int k = index1; k < index1 + len; k++)
+                {
+                    sum = sum + *(nums + k);
+                    // std::cout << "\nSum of index " << k << " with value " << *(nums+k) << " has value " << sum << std::endl;
+                }
             }
             
-            if (temp > max_sub){
-                max_sub = temp;
+            if (sum > max_sub){
+                max_sub = sum;
             }
         }
     }
 
-    // std::cout << "Max sub array has value " << max_sub << std::endl;
+    std::cout << "Max sub array has value " << max_sub << std::endl;
     // std::cout << "Max sub array lies between indexes " << index1 << " and " << index2 << std::endl;
 
     if(max_sub < 0){
--- main-2-2.cpp
+++ main-2-2.cpp
@@ -2,7 +2,7 @@ extern int max_sub_sum(int *nums,int length);
 
 int main(){
     int length = 5;
-    int array[] = {1,2,-3,4,5};
+    int array[] = {1,-2,-3,4,5};
 
     max_sub_sum(array, length);
 
